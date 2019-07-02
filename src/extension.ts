e21b52075a6446a6806e5eb92fece3a7aab42733
lizpen <stosc@sidaxin.com>
2019-07-02 22:18:11 +0800

    逐步修改

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,55 @@
-'use strict';
+"use strict";
 
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-import { DepNodeProvider, Dependency } from './nodeDependencies';
-import { JsonOutlineProvider } from './jsonOutline';
-import { FtpExplorer } from './ftpExplorer';
-import { FileExplorer } from './fileExplorer';
-import { TestView } from './testView';
+import { JsonOutlineProvider } from "./jsonOutline";
+import { GiteeReopProvider, GiteeReop } from "./gitee";
 
 export function activate(context: vscode.ExtensionContext) {
+  //gitee
+  const giteeReopProvider = new GiteeReopProvider(vscode.workspace.rootPath);
+  vscode.window.registerTreeDataProvider("giteeReops", giteeReopProvider);
+  vscode.commands.registerCommand("nodeDependencies.refreshEntry", () =>
+    giteeReopProvider.refresh()
+  );
+  vscode.commands.registerCommand("extension.openPackageOnNpm", moduleName =>
+    vscode.commands.executeCommand(
+      "vscode.open",
+      vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)
+    )
+  );
+  vscode.commands.registerCommand("nodeDependencies.addEntry", () =>
+    vscode.window.showInformationMessage(`Successfully called add entry.`)
+  );
+  vscode.commands.registerCommand(
+    "nodeDependencies.editEntry",
+    (node: GiteeReop) =>
+      vscode.window.showInformationMessage(
+        `Successfully called edit entry on ${node.label}.`
+      )
+  );
+  vscode.commands.registerCommand(
+    "nodeDependencies.deleteEntry",
+    (node: GiteeReop) =>
+      vscode.window.showInformationMessage(
+        `Successfully called delete entry on ${node.label}.`
+      )
+  );
 
-	// Samples of `window.registerTreeDataProvider`
-	const nodeDependenciesProvider = new DepNodeProvider(vscode.workspace.rootPath);
-	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
-	vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
-	vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
-	vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
-	vscode.commands.registerCommand('nodeDependencies.editEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
-	vscode.commands.registerCommand('nodeDependencies.deleteEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
+  // Samples of `window.registerTreeDataProvider`
 
-	const jsonOutlineProvider = new JsonOutlineProvider(context);
-	vscode.window.registerTreeDataProvider('jsonOutline', jsonOutlineProvider);
-	vscode.commands.registerCommand('jsonOutline.refresh', () => jsonOutlineProvider.refresh());
-	vscode.commands.registerCommand('jsonOutline.refreshNode', offset => jsonOutlineProvider.refresh(offset));
-	vscode.commands.registerCommand('jsonOutline.renameNode', offset => jsonOutlineProvider.rename(offset));
-	vscode.commands.registerCommand('extension.openJsonSelection', range => jsonOutlineProvider.select(range));
-
-	// Samples of `window.createView`
-	new FtpExplorer(context);
-	new FileExplorer(context);
-
-	// Test View
-	new TestView(context);
+  const jsonOutlineProvider = new JsonOutlineProvider(context);
+  vscode.window.registerTreeDataProvider("jsonOutline", jsonOutlineProvider);
+  vscode.commands.registerCommand("jsonOutline.refresh", () =>
+    jsonOutlineProvider.refresh()
+  );
+  vscode.commands.registerCommand("jsonOutline.refreshNode", offset =>
+    jsonOutlineProvider.refresh(offset)
+  );
+  vscode.commands.registerCommand("jsonOutline.renameNode", offset =>
+    jsonOutlineProvider.rename(offset)
+  );
+  vscode.commands.registerCommand("extension.openJsonSelection", range =>
+    jsonOutlineProvider.select(range)
+  );
 }
---
+++ src/gitee.ts
@@ -0,0 +1,147 @@
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+
+export class GiteeReopProvider implements vscode.TreeDataProvider<GiteeReop> {
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    GiteeReop | undefined
+  > = new vscode.EventEmitter<GiteeReop | undefined>();
+  readonly onDidChangeTreeData: vscode.Event<GiteeReop | undefined> = this
+    ._onDidChangeTreeData.event;
+
+  constructor(private workspaceRoot: string | undefined) {}
+
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element: GiteeReop): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: GiteeReop): Thenable<GiteeReop[]> {
+    if (!this.workspaceRoot) {
+      vscode.window.showInformationMessage("No dependency in empty workspace");
+      return Promise.resolve([]);
+    }
+
+    if (element) {
+      return Promise.resolve(
+        this.getDepsInPackageJson(
+          path.join(
+            this.workspaceRoot,
+            "node_modules",
+            element.label,
+            "package.json"
+          )
+        )
+      );
+    } else {
+      const packageJsonPath = path.join(this.workspaceRoot, "package.json");
+      if (this.pathExists(packageJsonPath)) {
+        return Promise.resolve(this.getDepsInPackageJson(packageJsonPath));
+      } else {
+        vscode.window.showInformationMessage("Workspace has no package.json");
+        return Promise.resolve([]);
+      }
+    }
+  }
+
+  /**
+   * Given the path to package.json, read all its dependencies and devDependencies.
+   */
+  private getDepsInPackageJson(packageJsonPath: string): GiteeReop[] {
+    if (this.pathExists(packageJsonPath)) {
+      const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+
+      const toDep = (moduleName: string, version: string): GiteeReop => {
+        if (
+          this.pathExists(
+            path.join(this.workspaceRoot, "node_modules", moduleName)
+          )
+        ) {
+          return new GiteeReop(
+            moduleName,
+            version,
+            vscode.TreeItemCollapsibleState.Collapsed
+          );
+        } else {
+          return new GiteeReop(
+            moduleName,
+            version,
+            vscode.TreeItemCollapsibleState.None,
+            {
+              command: "extension.openPackageOnNpm",
+              title: "",
+              arguments: [moduleName]
+            }
+          );
+        }
+      };
+
+      const deps = packageJson.dependencies
+        ? Object.keys(packageJson.dependencies).map(dep =>
+            toDep(dep, packageJson.dependencies[dep])
+          )
+        : [];
+      const devDeps = packageJson.devDependencies
+        ? Object.keys(packageJson.devDependencies).map(dep =>
+            toDep(dep, packageJson.devDependencies[dep])
+          )
+        : [];
+      return deps.concat(devDeps);
+    } else {
+      return [];
+    }
+  }
+
+  private pathExists(p: string): boolean {
+    try {
+      fs.accessSync(p);
+    } catch (err) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+export class GiteeReop extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    private version: string,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    public readonly command?: vscode.Command
+  ) {
+    super(label, collapsibleState);
+  }
+
+  get tooltip(): string {
+    return `${this.label}-${this.version}`;
+  }
+
+  get description(): string {
+    return this.version;
+  }
+
+  iconPath = {
+    light: path.join(
+      __filename,
+      "..",
+      "..",
+      "resources",
+      "light",
+      "dependency.svg"
+    ),
+    dark: path.join(
+      __filename,
+      "..",
+      "..",
+      "resources",
+      "dark",
+      "dependency.svg"
+    )
+  };
+
+  contextValue = "dependency";
+}
